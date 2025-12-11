a06183d4a82f9d6e5439db257e3abdb695846fa5
Shae <jan9103.wargear@protonmail.com>
2025-12-12 00:25:46 +0100

    feat: make span commands const-compatible

--- src/commands/here_span.rs
+++ src/commands/here_span.rs
@@ -38,10 +38,37 @@ impl Command for HereSpanCommand {
             None,
         ))
     }
+
+    fn is_const(&self) -> bool {
+        true
+    }
+
+    fn run_const(
+        &self,
+        _working_set: &StateWorkingSet,
+        call: &Call,
+        _input: PipelineData,
+    ) -> std::result::Result<PipelineData, ShellError> {
+        Ok(PipelineData::Value(
+            Value::range(
+                Range::new(
+                    Value::int(call.head.start as i64, call.head),
+                    Value::int(call.head.start as i64 + 1, call.head),
+                    Value::int(call.head.end as i64, call.head),
+                    nu_protocol::ast::RangeInclusion::Inclusive,
+                    call.head,
+                )?,
+                call.head,
+            ),
+            None,
+        ))
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
 pub struct GetSpanCommand;
+
+impl GetSpanCommand {}
 
 impl Command for GetSpanCommand {
     fn name(&self) -> &str {
@@ -91,6 +118,46 @@ impl Command for GetSpanCommand {
         };
         let span: Span = Span::new(range.0 as usize, range.1 as usize); // i don't use a 32bit system - not my problem
         let data: &[u8] = engine_state.get_span_contents(span);
+
+        Ok(PipelineData::Value(Value::binary(data, call.head), None))
+    }
+
+    fn is_const(&self) -> bool {
+        true
+    }
+
+    fn run_const(
+        &self,
+        working_set: &StateWorkingSet,
+        call: &Call,
+        _input: PipelineData,
+    ) -> std::result::Result<PipelineData, ShellError> {
+        let range = call.req_const::<Range>(working_set, 0)?;
+        let range: (i64, i64) = match range {
+            Range::IntRange(int_range) => (
+                int_range.start(),
+                match int_range.end() {
+                    std::ops::Bound::Included(i) => i,
+                    std::ops::Bound::Excluded(i) => i - 1,
+                    std::ops::Bound::Unbounded => {
+                        return Err(ShellError::IncorrectValue {
+                            msg: String::from("span needs a end"),
+                            val_span: Span::unknown(),
+                            call_span: call.head,
+                        });
+                    }
+                },
+            ),
+            Range::FloatRange(_) => {
+                return Err(ShellError::IncorrectValue {
+                    msg: String::from("expected a integer range, got a float range"),
+                    val_span: Span::unknown(),
+                    call_span: call.head,
+                });
+            }
+        };
+        let span: Span = Span::new(range.0 as usize, range.1 as usize); // i don't use a 32bit system - not my problem
+        let data: &[u8] = working_set.get_span_contents(span);
 
         Ok(PipelineData::Value(Value::binary(data, call.head), None))
     }
